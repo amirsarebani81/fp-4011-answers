fbc0d2f11a90a939cdc0400c7985c64456f9bd45
MohammadSadegh Ghobadian <msghobadian80@gmail.com>
2022-12-28 01:33:16 +0330

    remove redundant methods

--- homework-3/q4/number_pattern.c
+++ homework-3/q4/number_pattern.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
 
-void print_descending_part(int n, int number) {
-    for (int i = 0; i < n; i++)
-        printf("%d", number - i);
-}
-
-void print_repeating_part(int number) {
-    for (int i = 0; i < number; i++)
-        printf("%d", number);
-}
-
-void print_row(int biggestNumber, int number) {
-    int n = biggestNumber - number;
-    print_descending_part(n, biggestNumber);
-    print_repeating_part(number);
-}
-
-void print_pattern(int number) {
-    for (int i = 0; i < number; i++) {
-        print_row(number, number - i);
+void print_pattern(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++)
+            printf("%d", n - j);
+        for (int j = 0; j < n - i; j++)
+            printf("%d", n - i);
         printf("\n");
     }
 }
